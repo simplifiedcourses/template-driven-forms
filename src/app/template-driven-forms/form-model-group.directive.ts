53d69fa618336700fdfc98009c6ac1bea884318b
simplifiedcourses <brecht@simplified.courses>
2024-02-26 19:07:16 +0100

    Significant performance optimisations, regarding the amount of vest suites being triggered

--- src/app/template-driven-forms/form-model-group.directive.ts
+++ src/app/template-driven-forms/form-model-group.directive.ts
@@ -1,7 +1,9 @@
-import { Directive, inject } from '@angular/core';
+import { Directive, NgZone, inject, signal } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 import { FormDirective } from './form.directive';
 import { createValidator, getFormGroupField } from './utils';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ReplaySubject, tap, debounceTime, delay, of } from 'rxjs';
 
 @Directive({
   selector: '[ngModelGroup]',
@@ -12,14 +14,47 @@ import { createValidator, getFormGroupField } from './utils';
 })
 export class FormModelGroupDirective implements Validator {
   private readonly formDirective = inject(FormDirective);
+  private readonly debounced = signal(false);
+  private readonly last = signal<ValidationErrors | null>(null);
+  private readonly control$$ = new ReplaySubject<AbstractControl<any, any>>(0);
+  private readonly ngZone = inject(NgZone);
+
+  public constructor() {
+    /**
+     * Ignore all validations until the debounce time had been exceeded
+     */
+    this.ngZone.runOutsideAngular(() => {
+      this.control$$
+        .pipe(
+          tap(() => this.debounced.set(false)),
+          debounceTime(0),
+          takeUntilDestroyed(),
+        )
+        .subscribe((control) => {
+          this.debounced.set(true);
+          control.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+        });
+    });
+  }
+
 
   public validate(control: AbstractControl): ValidationErrors | null {
+    if (!this.debounced()) {
+      this.control$$.next(control);
+      // we keep the last value in a signal because Angular triggers the validator twice
+      // and we don't want to have a flickering, so we keep track of the last value
+      return this.last();
+    }
+    this.debounced.set(false);
+
     const { ngForm, suite, formValue } = this.formDirective;
     if (!suite || !formValue) {
       throw new Error('suite or formValue is missing');
     }
     const field = getFormGroupField(ngForm.control, control);
     const validator = createValidator(field, formValue, suite);
-    return validator(control);
+    const res = validator(control);
+    this.last.set(res);
+    return res;
   }
 }
--- src/app/template-driven-forms/form-model.directive.ts
+++ src/app/template-driven-forms/form-model.directive.ts
@@ -1,8 +1,9 @@
-import { Directive, inject } from '@angular/core';
+import { Directive, Input, NgZone, inject, signal } from '@angular/core';
 import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
 import { FormDirective } from './form.directive';
 import { createAsyncValidator, getFormControlField } from './utils';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject, debounceTime, delay, of, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 
 @Directive({
@@ -14,13 +15,47 @@ import { Observable } from 'rxjs';
 })
 export class FormModelDirective implements AsyncValidator {
   private readonly formDirective = inject(FormDirective);
+  private readonly debounced = signal(false);
+  private readonly last = signal<ValidationErrors | null>(null);
+  private readonly control$$ = new ReplaySubject<AbstractControl<any, any>>(0);
+  private readonly ngZone = inject(NgZone);
+  @Input() public  type: 'radio' |unknown| undefined = undefined;
+
+  public constructor() {
+    /**
+     * Ignore all validations until the debounce time had been exceeded
+     */
+    this.ngZone.runOutsideAngular(() => {
+      this.control$$
+        .pipe(
+          tap(() => this.debounced.set(false)),
+          debounceTime(0),
+          takeUntilDestroyed(),
+        )
+        .subscribe((control) => {
+          this.debounced.set(true);
+          control.updateValueAndValidity({ onlySelf: true, emitEvent: false });
+        });
+    });
+  }
 
   public validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    if (!this.debounced() && this.type !== 'radio') {
+      this.control$$.next(control);
+      // we keep the last value in a signal because Angular triggers the validator twice
+      // and we don't want to have a flickering, so we keep track of the last value
+      // We also want to delay the last value to avoid flickering of an error that was 
+      // shown previously
+      return of(this.last()).pipe(delay(0));
+    }
+    this.debounced.set(false);
     const { ngForm, suite, formValue } = this.formDirective;
     if (!suite || !formValue) {
       throw new Error('suite or formValue is missing');
     }
     const field = getFormControlField(ngForm.control, control);
-    return createAsyncValidator(field, formValue, suite)(control) as Observable<ValidationErrors|null>;
+    return (createAsyncValidator(field, formValue, suite)(control) as Observable<ValidationErrors | null>).pipe(
+      tap((v) => this.last.set(v)),
+    );
   }
 }
