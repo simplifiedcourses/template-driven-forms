381c08db20d1666de25b69a9ff81182777ddd502
simplifiedcourses <brecht@simplified.courses>
2024-03-05 18:10:16 +0100

    Optimized control wrapper

--- src/app/template-driven-forms/control-wrapper/control-wrapper.component.ts
+++ src/app/template-driven-forms/control-wrapper/control-wrapper.component.ts
@@ -1,6 +1,6 @@
 import { Component, ContentChild, HostBinding, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgModel, NgModelGroup } from '@angular/forms';
+import { AbstractControl, NgModel, NgModelGroup } from '@angular/forms';
 
 @Component({
   selector: '[scControlWrapper]',
@@ -10,18 +10,31 @@ import { NgModel, NgModelGroup } from '@angular/forms';
   styleUrls: ['./control-wrapper.component.scss']
 })
 export class ControlWrapperComponent {
-  @ContentChild(NgModel) public ngModel?: NgModel;
+  // Cache the previous error to avoi 'flickering'
+  private previousError?: string[];
+  @ContentChild(NgModel) public ngModel?: NgModel; // Optional ngModel
+
+  // Optional ngModelGroup
   public readonly ngModelGroup: NgModelGroup | null = inject(NgModelGroup, {
     optional: true,
     self: true,
   });
 
+  private get control(): AbstractControl|undefined {
+    return this.ngModelGroup ? this.ngModelGroup.control : this.ngModel?.control;
+  }
+
   @HostBinding('class.input-wrapper--invalid')
   public get invalid() {
-    if ((!this.ngModelGroup && this.ngModel?.control?.errors && (this.ngModel?.touched || this.ngModel?.dirty))
-      || (this.ngModelGroup?.control?.errors && (this.ngModelGroup.touched || this.ngModelGroup?.dirty))) {
-      return true;
+    return this.control?.touched && this.previousError;
+  }
+
+  public get errors(): string[] | undefined {
+    if (this.control?.pending) {
+      return this.previousError;
+    } else {
+      this.previousError = this.control?.errors?.['errors'];
     }
-    return false;
+    return this.control?.errors?.['errors'];
   }
 }
